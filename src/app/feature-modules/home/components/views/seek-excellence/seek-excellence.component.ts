5a39b7aaa976104bc18d2d027b04c78773fdfcff
IsaquiasMarques <patisaquias2000@gmail.com>
2024-07-03 22:15:57 +0100

    checkout count header and seek-excellence sections

--- src/app/feature-modules/home/components/views/seek-excellence/seek-excellence.component.ts
+++ src/app/feature-modules/home/components/views/seek-excellence/seek-excellence.component.ts
@@ -7,77 +7,138 @@ import { ImagePath } from '@core/data/models/image-path.model';
   styleUrl: './seek-excellence.component.css'
 })
 export class SeekExcellenceComponent {
-
-  images: ImagePath[] = [
-    {
-      genericPath: 'assets/images/home/we-seek-excellence/image-1.png',
-      allSizes: {
-        '1536x1536': 'assets/images/home/we-seek-excellence/image-1.png',
-        '1536x1536-height': 4096,
-        '1536x1536-width': 1996,
-        '2048x2048': 'assets/images/home/we-seek-excellence/image-1.png',
-        '2048x2048-height': 4096,
-        '2048x2048-width': 1996,
-        large: 'assets/images/home/we-seek-excellence/image-1.png',
-        'large-height': 4096,
-        'large-width': 1996,
-        medium: 'assets/images/home/we-seek-excellence/image-1.png',
-        'medium-height': 4096,
-        'medium-width': 1996,
-        medium_large: 'assets/images/home/we-seek-excellence/image-1.png',
-        'medium_large-height': 4096,
-        'medium_large-width': 1996,
-        thumbnail: 'assets/images/home/we-seek-excellence/image-1.png',
-        'thumbnail-height': 4096,
-        'thumbnail-width': 1996
-      }
-    },
-    {
-      genericPath: 'assets/images/home/we-seek-excellence/image-2.png',
-      allSizes: {
-        '1536x1536': 'assets/images/home/we-seek-excellence/image-2.png',
-        '1536x1536-height': 4096,
-        '1536x1536-width': 1785,
-        '2048x2048': 'assets/images/home/we-seek-excellence/image-2.png',
-        '2048x2048-height': 4096,
-        '2048x2048-width': 1785,
-        large: 'assets/images/home/we-seek-excellence/image-2.png',
-        'large-height': 4096,
-        'large-width': 1785,
-        medium: 'assets/images/home/we-seek-excellence/image-2.png',
-        'medium-height': 4096,
-        'medium-width': 1785,
-        medium_large: 'assets/images/home/we-seek-excellence/image-2.png',
-        'medium_large-height': 4096,
-        'medium_large-width': 1785,
-        thumbnail: 'assets/images/home/we-seek-excellence/image-2.png',
-        'thumbnail-height': 4096,
-        'thumbnail-width': 1785
-      }
-    },
-  ];
   activeIndex: number = 0;
+  activeBiggerIndexItem: number = 0;
+
+  productDetailed: { product: string, images: ImagePath[] }[] = [
+    {
+      product: 't-shirt',
+      images: [
+        {
+          genericPath: 'assets/images/home/we-seek-excellence/image-1.png',
+          allSizes: {
+            '1536x1536': 'assets/images/home/we-seek-excellence/image-1.png',
+            '1536x1536-height': 4096,
+            '1536x1536-width': 1996,
+            '2048x2048': 'assets/images/home/we-seek-excellence/image-1.png',
+            '2048x2048-height': 4096,
+            '2048x2048-width': 1996,
+            large: 'assets/images/home/we-seek-excellence/image-1.png',
+            'large-height': 4096,
+            'large-width': 1996,
+            medium: 'assets/images/home/we-seek-excellence/image-1.png',
+            'medium-height': 4096,
+            'medium-width': 1996,
+            medium_large: 'assets/images/home/we-seek-excellence/image-1.png',
+            'medium_large-height': 4096,
+            'medium_large-width': 1996,
+            thumbnail: 'assets/images/home/we-seek-excellence/image-1.png',
+            'thumbnail-height': 4096,
+            'thumbnail-width': 1996
+          }
+        },
+        {
+          genericPath: 'assets/images/home/we-seek-excellence/image-2.png',
+          allSizes: {
+            '1536x1536': 'assets/images/home/we-seek-excellence/image-2.png',
+            '1536x1536-height': 4096,
+            '1536x1536-width': 1785,
+            '2048x2048': 'assets/images/home/we-seek-excellence/image-2.png',
+            '2048x2048-height': 4096,
+            '2048x2048-width': 1785,
+            large: 'assets/images/home/we-seek-excellence/image-2.png',
+            'large-height': 4096,
+            'large-width': 1785,
+            medium: 'assets/images/home/we-seek-excellence/image-2.png',
+            'medium-height': 4096,
+            'medium-width': 1785,
+            medium_large: 'assets/images/home/we-seek-excellence/image-2.png',
+            'medium_large-height': 4096,
+            'medium_large-width': 1785,
+            thumbnail: 'assets/images/home/we-seek-excellence/image-2.png',
+            'thumbnail-height': 4096,
+            'thumbnail-width': 1785
+          }
+        },
+      ]
+    },
+    {
+      product: 'short',
+      images: [
+        {
+          genericPath: 'assets/images/home/we-seek-excellence/image-3.png',
+          allSizes: {
+            '1536x1536': 'assets/images/home/we-seek-excellence/image-3.png',
+            '1536x1536-height': 2860,
+            '1536x1536-width': 2472,
+            '2048x2048': 'assets/images/home/we-seek-excellence/image-3.png',
+            '2048x2048-height': 2860,
+            '2048x2048-width': 2472,
+            large: 'assets/images/home/we-seek-excellence/image-3.png',
+            'large-height': 2860,
+            'large-width': 2472,
+            medium: 'assets/images/home/we-seek-excellence/image-3.png',
+            'medium-height': 2860,
+            'medium-width': 2472,
+            medium_large: 'assets/images/home/we-seek-excellence/image-3.png',
+            'medium_large-height': 2860,
+            'medium_large-width': 2472,
+            thumbnail: 'assets/images/home/we-seek-excellence/image-3.png',
+            'thumbnail-height': 2860,
+            'thumbnail-width': 2472
+          }
+        },
+        {
+          genericPath: 'assets/images/home/we-seek-excellence/image-4.png',
+          allSizes: {
+            '1536x1536': 'assets/images/home/we-seek-excellence/image-4.png',
+            '1536x1536-height': 3392,
+            '1536x1536-width': 3184,
+            '2048x2048': 'assets/images/home/we-seek-excellence/image-4.png',
+            '2048x2048-height': 3392,
+            '2048x2048-width': 3184,
+            large: 'assets/images/home/we-seek-excellence/image-4.png',
+            'large-height': 3392,
+            'large-width': 3184,
+            medium: 'assets/images/home/we-seek-excellence/image-4.png',
+            'medium-height': 3392,
+            'medium-width': 3184,
+            medium_large: 'assets/images/home/we-seek-excellence/image-4.png',
+            'medium_large-height': 3392,
+            'medium_large-width': 3184,
+            thumbnail: 'assets/images/home/we-seek-excellence/image-4.png',
+            'thumbnail-height': 3392,
+            'thumbnail-width': 3184
+          }
+        },
+      ]
+    }
+  ];
 
   showImage(index: number){
     this.activeIndex = index;
   }
 
+  changeProduct(index: number){
+    this.activeBiggerIndexItem = index;
+  }
+
   prev(){
-    if(this.activeIndex === 0){
+    if(this.activeBiggerIndexItem === 0){
       return;
     }else{
-      this.activeIndex--;
+      this.activeBiggerIndexItem--;
     }
-    this.showImage(this.activeIndex);
+    this.showImage(0);
   }
 
   next(){
-    if(this.activeIndex === this.images.length - 1){
+    if(this.activeBiggerIndexItem === this.productDetailed[this.activeBiggerIndexItem].images.length - 1){
       return;
     }else{
-      this.activeIndex++;
+      this.activeBiggerIndexItem++;
     }
-    this.showImage(this.activeIndex);
+    this.showImage(0);
   }
 
 }
